a10620fdacc1c575f35b603ba3f8ce3bf2cd88b7
Surbhi Jadhao <surbhijadhao89@gmail.com>
2022-04-14 22:19:12 +0530

    entites added

---
+++ ClothBazar.Entity/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Entity
+{
+    public class Product : BaseEntity
+    {
+        public Category Category { get; set; } //creating object to define relation with category
+        public Decimal Price { get; set; } // float store approximate value while decimal exact value,
+                                           // money - decimal, scientific measurement- float
+        //public int Size { get; set; }
+
+    }
+}
